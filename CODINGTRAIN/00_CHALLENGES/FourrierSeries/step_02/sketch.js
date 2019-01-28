2d723f39d02c28d20a8f986bf8d9914200159351
Yann Depps <yann.deperrot@protonmail.com>
2019-01-28 23:36:41 +0100

    finished fourrier series

--- CODINGTRAIN/00_CHALLENGES/FourrierSeries/step_02/sketch.js
+++ CODINGTRAIN/00_CHALLENGES/FourrierSeries/step_02/sketch.js
@@ -2,45 +2,56 @@
 // angle or time : time is moving forward
 let time = 0;
 let wave = [];
+let slider;
 
 function setup() {
     createCanvas(600, 400);
+    slider = createSlider(1,10,1);
 }
 
 function draw() {
     background(0);
-
-    // a circle
-    translate(200, 200);
-    let radius = 100;
-    stroke(255);
-    noFill();
-    ellipse(0, 0, radius*2);
+    translate(150, 200);
 
     // polar coordinates
-    let x = radius * cos(time);
-    let y = radius * sin(time);
-    // every time we calculate a new y value, store it at the start of the array
-    wave.unshift(y);
-    fill(255);
-    line(0, 0, x, y);
-    ellipse(x, y, 8);
+    let x = 0;
+    let y = 0;
 
-    // use the array and draw the value of y
-    beginShape();
+    for (let i = 0; i < slider.value(); i++) {
+        let prevx = x;
+        let prevy = y;
+        let n = i * 2 + 1;
+        let radius = 75 * (4 / (n * PI));
+        x += radius * cos(n * time);
+        y += radius * sin(n * time);
+
+        stroke(255, 100);
+        noFill();
+        ellipse(prevx, prevy, radius*2);   
+
+        stroke(255, 200);
+        line(prevx, prevy, x, y);
+        fill(255);
+        ellipse(x, y, 4);
+   
+    
+    }
+
+    wave.unshift(y);
+
     translate(200, 0);
-    // connect a line from circle to wave
     line(x-200, y, 0, wave[0]);
+
+    beginShape();
     noFill();
     for (let i = 0; i < wave.length; i++) {
        vertex(i, wave[i]); 
     }
     endShape();
-    // time is moving forward
+    
     time += 0.05;
 
-    // do not draw too much points
-    if (wave.length > 150) {
+    if (wave.length > 175) {
         wave.pop();
     }
 }
